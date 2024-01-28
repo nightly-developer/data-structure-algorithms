658acdd579360cead1084296c30eb533bd6614d5
Vaibhav Kesarkar <85058107+nightly-developer@users.noreply.github.com>
2024-01-28 21:00:55 +0530

    Add files via upload

---
+++ Data_Structures_and_Algorithms/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        BinaryTree node = new BinaryTree(0);
+        System.out.println(node.data);
+        node.printInOrder();
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+  public static void main(String[] args) {
+    Node node = new Node(5);
+node.printInOrder();
+  }
+}
