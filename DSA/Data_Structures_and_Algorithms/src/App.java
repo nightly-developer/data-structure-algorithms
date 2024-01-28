ab3128ba5e4f56a99b313c2ce8f2cac44ae704b4
Vaibhav Kesarkar <85058107+nightly-developer@users.noreply.github.com>
2024-01-28 20:55:27 +0530

    Delete DSA directory

--- DSA/Data_Structures_and_Algorithms/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        BinaryTree node = new BinaryTree(0);
-        System.out.println(node.data);
-        node.printInOrder();
-
-    }
-}
--- DSA/Main.java
+++
@@ -1,6 +0,0 @@
-class Main{
-  public static void main(String[] args) {
-    Node node = new Node(5);
-node.printInOrder();
-  }
-}
